5a3b2315765c585af961d6d392a969d31320d0bf
Chris Tweedie <cltweedie@gmail.com>
2016-03-09 16:36:22 +0000

    Add some recipes to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create(
+  title: "Cauliflower and Spinach Pasta Bake".
+  link: "http://www.bbcgoodfood.com/recipes/166618/cauliflower-cheese-and-spinach-pasta-bakes",
+  image_url: "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--424655_11.jpg?itok=fAEOmPAx",
+  meal: "Dinner"
+)
+
+Recipe.create(
+  title: "Chocolate Brownies".
+  link: "http://www.bbcgoodfood.com/recipes/1223/bestever-brownies",
+  image_url: "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1001464_11.jpg?itok=Va_PfXJj",
+  meal: "Dessert"
+)
+
+Recipe.create(
+  title: "Toad in the Hole".
+  link: "http://www.bbcgoodfood.com/recipes/1572643/sams-toad-in-the-hole",
+  image_url: "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--736458_11.jpg?itok=x-pnYsyr",
+  meal: "Dinner"
+)
+
+Recipe.create(
+  title: "Wild Garlic Chicken Kiev".
+  link: "http://www.bbcgoodfood.com/recipes/wild-garlic-chicken-kiev",
+  image_url: "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/wild-garlic-chicken-kiev.jpg?itok=IhLpGrzG",
+  meal: "Dinner"
+)
